5a31c48f78b2e657956759cbb589fcef30252474
xpanxnex <pierre.klafs@gmx.de>
2018-01-09 18:49:04 +0100

    Default XamPass Application

---
+++ XamPass/XamPass/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XamPass.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
