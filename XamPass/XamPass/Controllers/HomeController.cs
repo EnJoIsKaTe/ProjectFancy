ca28478b07a96ab5e80975115d7101f9cd09966c
Patrick Kilian <patri@DESKTOP-B2DE12M>
2018-02-28 12:49:34 +0100

    Small Changes

--- XamPass/XamPass/Controllers/HomeController.cs
+++ XamPass/XamPass/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using XamPass.Models;
+using XamPass.Models.ViewModels;
 using XamPass.Models.DataBaseModels;
 
 namespace XamPass.Controllers
